f2faf1e8689fa0749f11a16bccb46e62027885c0
Levi Bernard <levichristianbernard@gmail.com>
2020-07-31 08:32:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='calculator'>
+        <div className='display'>
+          <p className='display-text'>Hello World!</p>
+        </div>
+        <div className='row'>
+          <button className='function-btn span-two'>CE</button>
+          <button className='function-btn'>+/-</button>
+          <button className='operator-btn'>÷</button>
+        </div>
+        <div className='row'>
+          <button className='num-btn'>7</button>
+          <button className='num-btn'>8</button>
+          <button className='num-btn'>9</button>
+          <button className='operator-btn'>x</button>
+        </div>
+        <div className='row'>
+          <button className='num-btn'>4</button>
+          <button className='num-btn'>5</button>
+          <button className='num-btn'>6</button>
+          <button className='operator-btn'>-</button>
+        </div>
+        <div className='row'>
+          <button className='num-btn'>1</button>
+          <button className='num-btn'>2</button>
+          <button className='num-btn'>3</button>
+          <button className='operator-btn'>+</button>
+        </div>
+        <div className='row'>
+          <button className='num-btn span-two'>0</button>
+          <button className='num-btn'>.</button>
+          <button className='operator-btn'>=</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
