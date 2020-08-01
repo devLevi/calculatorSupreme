8830268e34714d64cb81ca3f244c594945590db5
Levi Bernard <levichristianbernard@gmail.com>
2020-08-01 10:49:32 -0500

    added some calculations and styles

--- src/App.js
+++ src/App.js
@@ -10,12 +10,52 @@ class App extends React.Component {
       result: '',
     }
   }
+
+  onClick = (button) => {
+    if (button === '=') {
+      this.calculate()
+    } else if (button === 'AC') {
+      this.reset()
+    } else if (button === 'C') {
+      this.backspace()
+    } else {
+      this.setState({
+        result: this.state.result + button,
+      })
+    }
+  }
+
+  calculate = () => {
+    try {
+      this.setState({
+        // eslint-disable-next-line
+        result: (eval(this.state.result) || '') + '',
+      })
+    } catch (e) {
+      this.setState({
+        result: 'error',
+      })
+    }
+  }
+
+  reset = () => {
+    this.setState({
+      result: '',
+    })
+  }
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1),
+    })
+  }
+
   render() {
     return (
       <div className='App'>
         <div className='calculator'>
-          <Display />
-          <KeyPad />
+          <Display result={this.state.result} />
+          <KeyPad onClick={this.onClick} />
         </div>
       </div>
     )
--- src/components/KeyPad.js
+++ src/components/KeyPad.js
@@ -4,31 +4,125 @@ class KeyPad extends Component {
   render() {
     return (
       <>
-        <button className='function-btn'>AC</button>
-        <button className='function-btn'>+/-</button>
-        <button className='function-btn'>%</button>
-        <button className='operator-btn'>÷</button>
+        <button
+          name='AC'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='function-btn'>
+          AC
+        </button>
+        <button
+          name='+/-'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='function-btn'>
+          +/-
+        </button>
+        <button
+          name='%'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='function-btn'>
+          %
+        </button>
+        <button
+          name='/'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='operator-btn'>
+          &divide;
+        </button>
 
-        <button className='num-btn'>7</button>
-        <button className='num-btn'>8</button>
-        <button className='num-btn'>9</button>
-        <button className='operator-btn'>x</button>
+        <button
+          name='7'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          7
+        </button>
+        <button
+          name='8'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          8
+        </button>
+        <button
+          name='9'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          9
+        </button>
+        <button
+          name='*'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='operator-btn'>
+          x
+        </button>
 
-        <button className='num-btn'>4</button>
-        <button className='num-btn'>5</button>
-        <button className='num-btn'>6</button>
-        <button className='operator-btn'>-</button>
+        <button
+          name='4'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          4
+        </button>
+        <button
+          name='5'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          5
+        </button>
+        <button
+          name='6'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          6
+        </button>
+        <button
+          name='7'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='operator-btn'>
+          -
+        </button>
 
-        <button className='num-btn'>1</button>
-        <button className='num-btn'>2</button>
-        <button className='num-btn'>3</button>
-        <button className='operator-btn'>+</button>
+        <button
+          name='1'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          1
+        </button>
+        <button
+          name='2'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          2
+        </button>
+        <button
+          name='3'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          3
+        </button>
+        <button
+          name='+'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='operator-btn'>
+          +
+        </button>
 
-        <button className='num-btn' id='span-two'>
+        <button
+          name='0'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'
+          id='span-two'>
           0
         </button>
-        <button className='num-btn'>.</button>
-        <button className='operator-btn'>=</button>
+        <button
+          name='.'
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='num-btn'>
+          .
+        </button>
+        <button
+          name='='
+          onClick={(e) => this.props.onClick(e.target.name)}
+          className='operator-btn'>
+          =
+        </button>
       </>
     )
   }
